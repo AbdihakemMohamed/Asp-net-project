f4f9a1b1e9f492c7ad66ca9bbd871a81825134fe
abdihakemhp@gmail.com <AbdiHakem@DESKTOP-C1H4MTN>
2024-01-25 17:46:50 +0300

    Rent

---
+++ test/Pages/rent/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bike_Ms.Pages.rent
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
